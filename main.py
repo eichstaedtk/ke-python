c799acf0362a1e332d1db3a7751ac8c6d717afa5
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2021-04-30 16:42:14 +0200

    Add Pandas and Working to project

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
+import getopt
+import sys
+
 import pingpong as pp
+import pandas as pd
+
+
+def work():
+    print('Start Working')
+    data = pd.read_csv('countries.csv', ',')
+    print(data.head())
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    pp.start()
+
+    print('Command Lines Arguments '+str(len(sys.argv)))
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "e:", ["execute"])
+    except getopt.GetoptError:
+        print
+        'start.py -e game | work'
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-e':
+            if arg == 'game':
+                pp.start()
+            elif arg == 'work':
+                work()
+
+    print('main.py -e game | work')
