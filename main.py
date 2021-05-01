a9f7872a81716b3c813db6a2fb3d8b13071f7471
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2021-05-01 09:25:24 +0200

    Add more task for excercise 4

--- main.py
+++ main.py
@@ -10,6 +10,25 @@ def work():
     data = pd.read_csv('countries.csv', ',')
     print(data.head())
 
+    # Display some basic information as rows, columns and some basic statistical info.
+    print(data.describe())
+
+    # show the last 4 rows of the data frame.
+    print(data[len(data)-4:])
+
+    # Show all the row of countries who have the EURO
+    print(data.query('Currency == "EUR"'))
+
+    # Show only name and Currency in a new data frame
+    data2 = data[["Name", "Currency"]]
+    print(data2)
+
+    # Show only the rows/countries that have more than 2000 BIP (it is in Milliarden USD Bruttoinlandsprodukt)
+    print(data.query('BIP > 2000')["Name"])
+
+    # Select all countries where with inhabitants between 50 and 150 Mio
+    print(data.query('People > 50000000 and People < 150000000'))
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
