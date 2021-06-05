2fbdae62237bfdb45448f37c33d122dd9908237e
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2021-06-05 14:50:29 +0200

    Add Exercise 11

--- decisiontree.py
+++ decisiontree.py
@@ -8,7 +8,7 @@ buycomputer = pd.read_csv('buycomputer.csv', sep=';')
 le = ce.OrdinalEncoder(cols=['Age', 'Income', 'Student', 'Credit_rating'])
 
 Y = buycomputer['Buys_computer'].to_numpy()
-X = le.fit_transform(buycomputer[['Age', 'Income', 'Student', 'Credit_rating']])
+X = le.fit_transform(buycomputer[['Age', 'Income', 'Student', 'Credit_rating']]).to_numpy()
 
 print(buycomputer)
 
