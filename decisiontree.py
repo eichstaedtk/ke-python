f512230bc2cd2ad1412f7c3a6fd727a2759b8801
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2021-06-05 14:18:55 +0200

    Add Exercise 11

--- decisiontree.py
+++ decisiontree.py
@@ -2,14 +2,11 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import preprocessing
 from sklearn.tree import export_graphviz
-import numpy as np
-from sklearn import tree
+import category_encoders as ce
 
 buycomputer = pd.read_csv('buycomputer.csv', sep=';')
 
-le = preprocessing.OrdinalEncoder()
-
-#buycomputer = buycomputer.apply(le.fit_transform)
+le = ce.OrdinalEncoder(cols=['Age', 'Income', 'Student', 'Credit_rating'])
 
 Y = buycomputer['Buys_computer'].to_numpy()
 X = le.fit_transform(buycomputer[['Age', 'Income', 'Student', 'Credit_rating']])
@@ -19,10 +16,7 @@ print(buycomputer)
 print(Y)
 print(X)
 
-tree_clf = DecisionTreeClassifier(class_weight=None, criterion='entropy', max_depth=5, max_features=None, max_leaf_nodes=None,
-            min_impurity_decrease=0.0, min_impurity_split=None,
-            min_samples_leaf=1, min_samples_split=2,
-            min_weight_fraction_leaf=0.0, random_state=12345, splitter='best')
+tree_clf = DecisionTreeClassifier(class_weight=None, criterion='entropy', max_depth=10, splitter='best')
 
 tree_clf.fit(X, Y)
 
@@ -31,5 +25,5 @@ export_graphviz(
          out_file="buycomputer.dot",
          feature_names=['Age', 'Income', 'Student', 'Credit_rating'],
          rounded=True,
-         filled=True
+         filled=True,
  )
