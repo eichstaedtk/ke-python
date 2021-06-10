6b03df8c4356ec69825dd521ce14a44668b61e61
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2021-06-10 20:26:52 +0200

    Add Exercise 11 and 12

---
+++ k-means.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+dataset = pd.read_csv('edlich-kmeans.csv', skiprows=1)
+
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+    kmeans.fit(dataset.to_numpy())
+    # inertia method returns wcss for that model
+    wcss.append(kmeans.inertia_)
+
+
+plt.figure(figsize=(10,5))
+sns.lineplot(range(1, 11), wcss,marker='o',color='red')
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
