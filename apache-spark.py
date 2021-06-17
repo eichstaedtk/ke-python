ffed298f01ef7578d474d640dd6f27691c1aef49
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2021-06-17 22:04:38 +0200

    Add Exercise 13

---
+++ apache-spark.py
@@ -0,0 +1,26 @@
+import sys
+from pyspark import SparkContext, SparkConf
+import collections
+
+if __name__ == "__main__":
+    # create Spark context with necessary configuration
+    sc = SparkContext("local", "PySpark Word Count Exmaple")
+
+    # read data from text file and split each line into words
+    words = sc.textFile("t8.shakespeare.txt").map( lambda x: x.replace(',',' ').replace('.',' ').replace('-',' ').lower())
+
+    # count the occurrence of each word
+    counts = words.flatMap(lambda line: line.split(" ")) \
+        .map(lambda word: (word, 1)) \
+        .reduceByKey(lambda a, b: a + b)
+
+    # save the counts to output
+    sorted_counts = counts.sortBy(lambda wordCounts: wordCounts[1], ascending=False)
+    # the #24 most used word in Shakespeares writings
+    # the first one is not a word
+    i = 0
+    for word, count in sorted_counts.collect()[0:25]:
+        print("{} : {} : {} ".format(i, word, count))
+        i += 1
+
+
