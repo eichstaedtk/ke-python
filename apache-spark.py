feeb7e2648ada4ab9953504a762e8a261ba55a54
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2021-06-26 10:32:20 +0200

    Add Exercise 13

--- apache-spark.py
+++ apache-spark.py
@@ -2,8 +2,9 @@ from pyspark import SparkContext
 
 if __name__ == "__main__":
     # create Spark context with necessary configuration
-    sc = SparkContext("local", "PySpark Word Count Exmaple")
+    sc = SparkContext("local", "PySpark Word Count Master DataScience")
 
+    # read text file with spark context and remove not needed characters.
     shakespeare = sc.textFile("t8.shakespeare.txt").map(lambda x: x.replace(',', ' ').replace('.', ' ').replace('-', ' ').lower())
 
     # count the occurrence of each word
@@ -11,8 +12,10 @@ if __name__ == "__main__":
         .map(lambda word: (word, 1)) \
         .reduceByKey(lambda a, b: a + b)
 
+    # sort the result
     sorted_counts = counts.sortBy(lambda wordCounts: wordCounts[1], ascending=False)
 
+    # print the result using for loop
     i = 0
     for word, count in sorted_counts.collect()[0:25]:
         print("{} : {} : {} ".format(i, word, count))
