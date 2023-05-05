d4eaf08b9662dae4344fa0e35f7b321f361051f4
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2023-05-05 14:40:02 +0200

    Add new exersice for ML with more dimensions

--- pizzaml2.py
+++ pizzaml2.py
@@ -44,12 +44,4 @@ def graph(x, y):
     plt.show()
 
 
-X, Y = np.loadtxt("pizza.txt", skiprows=1, unpack=True)
 
-w, b = train(X, Y, iterations=10000, lr=0.01)
-print("\nw=%.3f, b=%.3f" % (w, b))
-
-# Predict the number of pizzas
-print("Prediction: x=%d => y=%.2f" % (20, predict(20, w, b)))
-
-graph(X, Y)
